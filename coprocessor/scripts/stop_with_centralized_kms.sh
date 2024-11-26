6a35bc81dcc6828156094903c7e921a7de3cbb4a
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2024-11-26 13:15:28 +0200

    fix: docker compose file path

--- coprocessor/scripts/stop_with_centralized_kms.sh
+++ coprocessor/scripts/stop_with_centralized_kms.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 sudo docker compose -vvv \
-    -f docker-compose/docker-compose-kms-base.yml \
-    -f docker-compose/docker-compose-kms-centralized.yml \
-    -f docker-compose/docker-compose-kms-gateway-centralized.yml \
-    -f docker-compose/docker-compose-coprocesor.yml \
+    -f ../docker-compose/docker-compose-kms-base.yml \
+    -f ../docker-compose/docker-compose-kms-centralized.yml \
+    -f ../docker-compose/docker-compose-kms-gateway-centralized.yml \
+    -f ../docker-compose/docker-compose-coprocesor.yml \
     down -v --remove-orphans
