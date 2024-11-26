64f9a094d53ca3ddeef00690dbeb175d699114d1
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2024-11-26 17:38:18 +0200

    build: remove migration container

--- coprocessor/scripts/stop_with_centralized_kms.sh
+++ coprocessor/scripts/stop_with_centralized_kms.sh
@@ -10,4 +10,5 @@ sudo docker compose -vvv \
     -f ../docker-compose/docker-compose-kms-centralized.yml \
     -f ../docker-compose/docker-compose-kms-gateway-centralized.yml \
     -f ../docker-compose/docker-compose-coprocesor.yml \
+    -f ../docker-compose/docker-compose-db-migration.yml \
     down -v --remove-orphans
