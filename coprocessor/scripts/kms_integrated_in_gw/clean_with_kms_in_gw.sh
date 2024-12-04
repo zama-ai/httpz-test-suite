bb79c924dc49ecad6230e5145f529e5159b95b5a
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2024-12-04 16:13:32 +0200

    chore: move prune to the end

--- coprocessor/scripts/kms_integrated_in_gw/clean_with_kms_in_gw.sh
+++ coprocessor/scripts/kms_integrated_in_gw/clean_with_kms_in_gw.sh
@@ -5,12 +5,12 @@ set -e
 # Import env variables from the .env file.
 export $(cat .env | xargs)
 
-sudo docker container prune -f
-sudo docker network prune -f
-
 sudo docker compose -vvv \
     -f ../../docker-compose/docker-compose-s3-mock.yml \
     -f ../../docker-compose/docker-compose-centralized-kms-integrated-in-gw.yml \
     -f ../../docker-compose/docker-compose-coprocesor.yml \
     -f ../../docker-compose/docker-compose-db-migration.yml \
     down -v --remove-orphans
+
+sudo docker container prune -f
+sudo docker network prune -f
