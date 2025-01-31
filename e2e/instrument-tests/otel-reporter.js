c8a4bb1dd18eef36e45e9562be0f6812bbf9cf02
Luis Montero <luismontero@hotmail.fr>
2025-01-31 15:24:38 +0100

    wip: all broken

--- e2e/instrument-tests/otel-reporter.js
+++ e2e/instrument-tests/otel-reporter.js
@@ -1,17 +1,27 @@
 // Custom Mocha reporter to create
-const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-http");
-const { MeterProvider } = require("@opentelemetry/sdk-metrics");
-const { Resource } = require("@opentelemetry/resources");
-const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+// Exporters
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 
+const { diag, DiagConsoleLogger, DiagLogLevel, trace, context } = require("@opentelemetry/api");
+const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-http");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
+
+const { envDetector, hostDetector, osDetector, processDetector, Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+
+const { NodeSDK } = require("@opentelemetry/sdk-node");
+
+const { MeterProvider, PeriodicExportingMetricReader, ConsoleMetricExporter } = require("@opentelemetry/sdk-metrics");
+const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
+
+// Auto
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 // const { awsEc2Detector, awsEksDetector } = require("@opentelemetry/resource-detector-aws");
 // const { containerDetector } = require("@opentelemetry/resource-detector-container");
-const { envDetector, hostDetector, osDetector, processDetector } = require("@opentelemetry/resources");
-const { NodeSDK } = require("@opentelemetry/sdk-node");
-const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require("@opentelemetry/sdk-metrics");
+
+// Mocha
 const Mocha = require("mocha");
 const {
   EVENT_SUITE_BEGIN,
@@ -23,48 +33,49 @@ const {
   EVENT_TEST_PASS,
   EVENT_TEST_FAIL,
 } = Mocha.Runner.constants;
-const { diag, DiagConsoleLogger, DiagLogLevel, trace } = require("@opentelemetry/api");
-const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
 
 // NOTE: debug open-telemetry logger
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-const sdk = new NodeSDK({
-  // Name of the service
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "e2e-test-suite",
-  }),
-  // spanProcessors: [new SimpleSpanProcessor(new ConsoleSpanExporter())],
-  // Use OpenTelemetry trace exporter
-  traceExporter: new OTLPTraceExporter(),
-  //   {
-  //   url: "http://localhost:4318", // Add your actual endpoint
-  //   headers: {}, // Add any headers if needed
-  // }
-  instrumentations: [
-    getNodeAutoInstrumentations({
-      // only instrument fs if it is part of another trace
-      "@opentelemetry/instrumentation-fs": {
-        requireParentSpan: true,
-      },
-    }),
-    new HttpInstrumentation({ requireParentSpan: true }),
-  ],
-  // Use OpenTelemetry metric exporter
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter(),
-  }),
-  resourceDetectors: [
-    envDetector,
-    hostDetector,
-    osDetector,
-    processDetector,
-    // containerDetector,
-    // awsEksDetector,
-    // awsEc2Detector,
-  ],
-});
-sdk.start();
+// const sdk = new NodeSDK({
+//   // Name of the service
+//   resource: new Resource({
+//     [SemanticResourceAttributes.SERVICE_NAME]: "e2e-test-suite",
+//   }),
+//   // Use OpenTelemetry trace exporter
+//   // traceExporter: new OTLPTraceExporter(),
+//   traceExporter: new OTLPTraceExporter(),
+//   //   {
+//   //   timeoutMillis: 15,
+//   //   // optional - default url is http://localhost:4318/v1/traces
+//   //   url: 'http://localhost:58539/v1/traces',
+//   //   // optional - collection of custom headers to be sent with each request, empty by default
+//   //   headers: {},
+//   // }
+//   instrumentations: [
+//     getNodeAutoInstrumentations({
+//       // only instrument fs if it is part of another trace
+//       "@opentelemetry/instrumentation-fs": {
+//         requireParentSpan: true,
+//       },
+//     }),
+//     new HttpInstrumentation(),
+//   ],
+//   // Use OpenTelemetry metric exporter
+//   metricReader: new PeriodicExportingMetricReader({
+//     exporter: new OTLPMetricExporter({ exportIntervalMillis: 10 }),
+//   }),
+//   resourceDetectors: [
+//     envDetector,
+//     hostDetector,
+//     osDetector,
+//     processDetector,
+//     // containerDetector,
+//     // awsEksDetector,
+//     // awsEc2Detector,
+//   ],
+// });
+// sdk.start();
 
 // Initialize OpenTelemetry Metrics
 const meterProvider = new MeterProvider({
@@ -95,10 +106,22 @@ const testDurationHistogram = meter.createHistogram("test.duration", {
 const testResultCounter = meter.createCounter("test.results", {
   description: "Test results by status",
 });
-const tracer = trace.getTracer("my-application", "0.1.0");
+
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: "some string",
+  [SemanticResourceAttributes.SERVICE_VERSION]: "some string",
+  [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: "some string",
+});
+
+const provider = new NodeTracerProvider({ resource });
+
+provider.addSpanProcessor(new BatchSpanProcessor(new OTLPTraceExporter()));
+provider.register();
+
+const tracer = provider.getTracer("some string", "some string");
 const tracing = true;
 
-// Create custom Mocha reporter
+// Create custom Mocha reporter that handles metrics
 class OTelReporter {
   constructor(runner) {
     const stats = runner.stats;
@@ -106,33 +129,31 @@ class OTelReporter {
     this.suites = new Map();
 
     runner.on(EVENT_TEST_BEGIN, (test) => {
-      console.log("EVENT_RUN_BEGIN");
-      console.log(EVENT_RUN_BEGIN);
-      if (tracing) {
-        const span = tracer.startSpan("test.execution", {
-          attributes: {
-            "test.name": test.title,
-            "test.suite": test.parent.title,
-            "test.framework": "mocha",
-          },
-        });
-        // NOTE: DEBUG
-        const activeSpan = trace.getActiveSpan();
-        console.log("Active span:", activeSpan);
+      console.log("EVENT_RUN_BEGIN begin");
 
-        this.tests.set(test, { test: test, span: span });
-      }
+      let activeSpan = trace.getActiveSpan();
+      console.log("Active span:", activeSpan);
 
-      console.log("EVENT_TEST_BEGIN");
-      console.log(test);
-      console.log(this.tests);
-      console.log();
+      const span = tracer.startActiveSpan("test.execution", {
+        attributes: {
+          "test.name": test.title,
+          "test.suite": test.parent.title,
+          "test.framework": "mocha",
+        },
+      });
+      trace.setSpan(context.active(), span);
+
+      activeSpan = trace.getActiveSpan();
+      console.log("Active span:", activeSpan);
+
+      this.tests.set(test, { test: test, span: span });
+      console.log("EVENT_RUN_BEGIN end");
     });
 
     runner.on(EVENT_TEST_END, (test) => {
       if (tracing) {
         console.log(this.tests.get(test));
-        console.log("CLOSING SPAN");
+        console.log("CLOSING TEST SPAN");
         console.log(this.tests.get(test).span);
         this.tests.get(test).span.end();
       }
@@ -141,35 +162,36 @@ class OTelReporter {
       console.log(test);
       console.log(this.tests);
       console.log();
+      testCounter.add(1);
     });
 
     runner.on(EVENT_SUITE_BEGIN, (suite) => {
-      if (tracing) {
-        const span = tracer.startSpan("test.execution", {
-          attributes: {
-            "suite.name": suite.title,
-            "test.framework": "mocha",
-          },
-        });
-        // NOTE: DEBUG
-        const activeSpan = trace.getActiveSpan();
-        console.log("Active span:", activeSpan);
-
-        this.suites.set(suite, { test: suite, span: span });
-      }
+      // if (tracing) {
+      //   const span = tracer.startSpan("test.execution", {
+      //     attributes: {
+      //       "suite.name": suite.title,
+      //       "test.framework": "mocha",
+      //     },
+      //   });
+      //   // NOTE: DEBUG
+      //   const activeSpan = trace.getActiveSpan();
+      //   console.log("Active span:", activeSpan);
+      //
+      //   this.suites.set(suite, { test: suite, span: span });
+      // }
       console.log("EVENT_SUITE_BEGIN");
       console.log(suite);
       console.log();
     });
 
     runner.on(EVENT_SUITE_END, (suite) => {
-      if (tracing) {
-        console.log(this.suites.get(suite));
-        console.log("CLOSING SPAN");
-        console.log(this.suites.get(suite).span);
-        this.suites.get(suite).span.end();
-      }
-
+      // if (tracing) {
+      //   console.log(this.suites.get(suite));
+      //   console.log("CLOSING SUITE SPAN");
+      //   console.log(this.suites.get(suite).span);
+      //   this.suites.get(suite).span.end();
+      // }
+      //
       console.log("EVENT_SUITE_END");
       console.log(suite);
       console.log();
